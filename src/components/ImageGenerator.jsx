90afc26bfcf43e967e8f5f9d7eddd01bc596807b
JassiSingh08 <singhjassi693@gmail.com>
2023-08-28 14:43:01 +0530

    Found API. Completed

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -5,32 +5,49 @@ import "./ImageGenerator.css"
 const ImageGenerator = () => {
 
   const [image_url, setImage_url] = useState('/')
+  const [loading, setLoading] = useState(false)
   const inputRef = useRef(null)
 
   //NEED TO FIND A FREE AI GENERATOR API // PAUSING PROJECT
 
   const GenerateImage = async () => {
-    if(inputRef.current.value === "") {
-      return  0
+    if (inputRef.current.value === "") {
+      return;
     }
-    const response = await fetch(
-      "https://api.deepai.org/api/text2img",{
-        text: `${inputRef.current.value}`,
-        method: "POST",
-        headers: {
-          "Api-key" : "8328e84f-d506-4a9b-8feb-d36f7474b267"
-        },
-      },
-    )
-    const data = await response.json()
-    console.log(data)
-  }
+    setLoading(true)
+    const data = inputRef.current.value;
+    try {
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/prompthero/openjourney",
+        {
+          headers: { Authorization: `Bearer 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` },
+          method: "POST",
+          body: JSON.stringify( data ), // Assuming the API expects the data in a specific format
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Request failed with status: " + response.status);
+      }
+      const blob = await response.blob(); // Convert the response to a binary blob
+      const imageUrl = URL.createObjectURL(blob); // Create a URL from the blob
+      setImage_url(imageUrl); // Update the state with the image URL
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   
   return (
     <div className="container">
       <div className="Gen-text">Ai Image <span>Generator</span></div>
       <div className="Gen-image">
         <img src={image_url === "/" ? default_img : image_url } alt="default_img" />
+      </div>
+
+      <div className="loading">
+        <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+        <div className={loading ? "loading-text" : "display-none"}>Loading...</div>
       </div>
 
       <div className="search-box">
